ae04a6e4f5a6fd3c81705f91821b0f912fee1f6a
jamesbiswell <43172281+jamesbiswell@users.noreply.github.com>
2026-03-25 18:51:28 +0000

    Adding section-02-junit-review

---
+++ 1.01-starting-project-solutions/section-02-junit-review/1.10-project-solution-conditional-tests/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
@@ -0,0 +1,52 @@
+package com.luv2code.junitdemo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.*;
+
+class ConditionalTest {
+
+    @Test
+    @Disabled("Don't run until JIRA #123 is resolved")
+    void basicTest() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testForWindowsOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    void testForMacOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_25)
+    @EnabledOnOs({ OS.MAC, OS.WINDOWS })
+    void testForMacAndWindowsOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledOnOs(OS.LINUX)
+    void testForLinuxOnly() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "LUV2CODE_ENV", matches = "DEV")
+    void testOnlyForDevEnvironment() {
+        // execute method and perform asserts
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "LUV2CODE_SYS_PROP", matches = "CI_CD_DEPLOY")
+    void testOnlyForSystemProperty() {
+        // execute method and perform asserts
+    }
+
+}
